a9663d6c32c97a7844cebf16fa305ce0a39a7eb9
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-16 10:12:04 +0530

    feat: add the main logic to bet

--- main.go
+++ main.go
@@ -31,7 +31,29 @@ func getBet(balance uint) uint {
 }
 
 func main() {
-	balance := 200
-
+	symbols := map[string]uint{
+		"A": 3,
+		"B": 7,
+		"C": 12,
+		"D": 20,
+	}
+	multipliers := map[string]uint{
+		"A": 20,
+		"B": 10,
+		"C": 5,
+		"D": 2,
+	}
+	balance := uint(200)
 	getName()
+
+	for balance > 0 {
+		bet := getBet(balance)
+		if bet == 0 {
+			break
+		}
+		balance -= bet
+
+	}
+
+	fmt.Printf("You have left with, $%d.\n", balance)
 }
