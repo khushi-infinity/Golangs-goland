bc195865cb5f4a6e3cd77fb2f0cab02f04e06a84
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-16 09:35:54 +0530

    The welcome message

--- hello.go
+++ hello.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func getName() string {
+	name := ""
+	fmt.Println("Welcome to the CASINO!")
+	fmt.Printf("Enter your name: ")
+	_, err := fmt.Scanln(&name)
+
+	if err != nil {
+		return ""
+	}
+	fmt.Printf("Welcome %s, Let's Play!\n", name)
+	return name
+}
+
 func main() {
-	fmt.Println("Hello World")
+	getName()
 }
